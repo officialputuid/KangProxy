b0559c889c4a181dc8531dab83e2f933d4ece668
officialputuid <officialputuid@hack.id>
2024-03-30 03:06:25 +0000

    KangProxy: Updates
    Signed-off-by: officialputuid <officialputuid@hack.id>

---
+++ xResults/clean.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+input_file="RAW.txt"
+output_file="new_RAW.txt"
+while IFS= read -r line; do
+    ip_port=$(echo "$line" | awk -F '[|:]' '{print $1 ":" $2}')
+    protocol=$(echo "$line" | awk -F '[|:]' '{print $3}')
+    case $protocol in
+        "socks4") protocol="socks4://";;
+        "socks5") protocol="socks5://";;
+        "http") protocol="http://";;
+        *) protocol="unknown://";;
+    esac
+    echo "${protocol}${ip_port}" >> "$output_file"
+done < "$input_file"
+
